f4b23c21efec61dffb61d1837fa2d72f30081f7e
bamidelemo <bamidelemo@gmail.com>
2021-11-10 20:58:32 +0100

    ...

--- components/DefineCapsForms.vue
+++ components/DefineCapsForms.vue
@@ -5,7 +5,7 @@
       <div class="my-2">
         <div class="my-2">Country:</div>
         <div>
-          <select class="w-full border rounded-md h-9">
+          <select class="w-full border rounded-md h-9 px-2">
             <option value="">Select Country</option>
           </select>
         </div>
@@ -27,13 +27,50 @@
             hover:bg-blue-500
           "
         >
-          Post CAP
+          Post Country CAP
         </button>
       </div>
     </form>
     <form v-if="whichform === 'TabState'">
       <h2 class="font-bold my-2">Define CAP for a State/Province</h2>
-      <div>...</div>
+      <div class="my-2">
+        <div class="my-2">Country where state/province is:</div>
+        <div>
+          <select class="w-full border rounded-md h-9 px-2">
+            <option value="">Select Country</option>
+          </select>
+        </div>
+      </div>
+      <div class="my-6">
+        <div class="my-2">State/Province:</div>
+        <div>
+          <input
+            type="text"
+            class="w-full border rounded-md h-9 px-2"
+            placeholder="Type State/Province Correctly"
+          />
+        </div>
+      </div>
+      <div class="my-6">
+        <div class="my-2">Attach Infographic:</div>
+        <div>
+          <input class="w-full border rounded-md" type="file" />
+        </div>
+      </div>
+      <div class="my-6">
+        <button
+          class="
+            w-full
+            rounded-xl
+            bg-blue-900
+            text-white
+            py-3
+            hover:bg-blue-500
+          "
+        >
+          Post State/Province CAP
+        </button>
+      </div>
     </form>
     <form v-if="whichform === 'TabCity'">
       <h2 class="font-bold my-2">Define CAP for a City/Municipality</h2>
